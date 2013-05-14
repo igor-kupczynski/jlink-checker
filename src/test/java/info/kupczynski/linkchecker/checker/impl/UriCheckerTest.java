e48bfb249b96deb85547ea801841420a5421dd6e
Igor Kupczynski <puszczyk@gmail.com>
2013-05-14 20:16:33 +0200

    Added more tests

--- src/test/java/info/kupczynski/linkchecker/checker/impl/UriCheckerTest.java
+++ src/test/java/info/kupczynski/linkchecker/checker/impl/UriCheckerTest.java
@@ -21,7 +21,6 @@ import com.google.common.collect.Lists;
 
 public class UriCheckerTest {
 
-	private static final boolean follow = true;
 	private static final String from = "http://example.com/";
 	private static final String to = "http://example.com/other";
 	private static final int depth = 3;
@@ -32,9 +31,30 @@ public class UriCheckerTest {
 		uriService = mockUriService();
 	}
 
+	@Test
+	public void assertCallsBackUriService() {
+		// With
+		UriChecker uriChecker = new UriChecker(uriService, true, from, to,
+				depth);
+		UriChecker.setUriFetcher(mockUriFetcher(mockSimplePage()));
+
+		ArgumentCaptor<UriStatusDTO> uriStatusDTOCaptor = ArgumentCaptor
+				.forClass(UriStatusDTO.class);
+
+		// Do
+		uriChecker.run();
+
+		// Assert
+		verify(uriService).finished(uriStatusDTOCaptor.capture());
+
+		UriStatusDTO status = uriStatusDTOCaptor.getValue();
+		assertThat(status.getHttpCode()).isEqualTo(Integer.valueOf(200));
+		assertThat(status.getStatus()).isEqualTo(UriStatusDTO.Code.OK);
+	}
+
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Test
-	public void asserFollowOnRedirect() {
+	public void assertFollowOnRedirect() {
 		// With
 		UriChecker uriChecker = new UriChecker(uriService, true, from, to,
 				depth);
@@ -61,6 +81,37 @@ public class UriCheckerTest {
 		assertThat(children).hasSize(1).contains(redirectTo);
 	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Test
+	public void assertFollowOnChildren() {
+		// With
+		UriChecker uriChecker = new UriChecker(uriService, true, from, to,
+				depth);
+		String item1 = "http://example.com/foo";
+		String item2 = "http://example.com/bar";
+		UriChecker
+				.setUriFetcher(mockUriFetcher(mockWithChildren(item1, item2)));
+
+		ArgumentCaptor<UriStatusDTO> uriStatusDTOCaptor = ArgumentCaptor
+				.forClass(UriStatusDTO.class);
+		ArgumentCaptor<Collection> childrenCaptor = ArgumentCaptor
+				.forClass(Collection.class);
+
+		// Do
+		uriChecker.run();
+
+		// Assert
+		verify(uriService).finished(uriStatusDTOCaptor.capture(),
+				childrenCaptor.capture());
+
+		UriStatusDTO status = uriStatusDTOCaptor.getValue();
+		assertThat(status.getHttpCode()).isEqualTo(Integer.valueOf(200));
+		assertThat(status.getStatus()).isEqualTo(UriStatusDTO.Code.OK);
+
+		Collection<String> children = childrenCaptor.getValue();
+		assertThat(children).hasSize(2).contains(item1).contains(item2);
+	}
+
 	private UriService mockUriService() {
 		UriService us = mock(UriService.class);
 		return us;
@@ -76,8 +127,17 @@ public class UriCheckerTest {
 		return uf;
 	}
 
+	private HttpResponseWrapper mockSimplePage() {
+		return mockHttpResponseWrapper(200, false,
+				Lists.<String> newArrayList());
+	}
+
 	private HttpResponseWrapper mockRedirect(String to) {
 		return mockHttpResponseWrapper(301, true, Lists.newArrayList(to));
+	}
+
+	private HttpResponseWrapper mockWithChildren(String... children) {
+		return mockHttpResponseWrapper(200, true, Lists.newArrayList(children));
 	}
 
 	private HttpResponseWrapper mockHttpResponseWrapper(int code,
